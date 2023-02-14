54a4687a244ec8ff37b666f9ac836814e5a068ef
John Doe <lucas.lopez.borchardt@gmail.com>
2023-02-14 16:45:10 +0100

    Delete functionality

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ListElement from './ListElement/ListElement'
 import Message from '../Message/Message'
 
-const List = ({filteredElements, weather}) => {
-
+const List = ({filteredElements, weather, onDeleteActivity}) => {
+  
 
   return (
     <>
@@ -14,8 +14,10 @@ const List = ({filteredElements, weather}) => {
       { filteredElements.map((e) => {
       return(
         <ListElement
+        listId={e.id}
         key={e.id}
         name={e.name}
+        onDeleteActivity={onDeleteActivity}
         />
       )
     })
