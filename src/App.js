9e2444297f89c7170cf9d807629cd8d9db52d6e3
daniel <daniel.enrriquesmercedes@gmail.com>
2023-02-14 10:58:16 +0100

    Form fuctionality created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import Form from "./components/Form/Form";
 
-
 function App() {
+
+  const [checked, setChecked] = useState(false);
+
+
+  function onAddActivity(e) {
+
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const simple_form = Object.fromEntries(form);
+
+    simple_form.isChecked = checked;
+
+    const formElement = e.target;
+    formElement.reset();
+
+    const nameInput =  e.target[0];
+    nameInput.focus();
+    
+  }
+
   return(    
-  <Form/>
+  <Form
+  onAddActivity={onAddActivity}
+  checked = {checked}
+  setChecked = {setChecked}
+  />
   )
 }
 
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,26 @@
 import React from "react";
 
-const Form = ({onAddActivity}) => {
+const Form = ({onAddActivity, checked, setChecked}) => {
+
+
   return (
     <>
-      <form>
+      <form onSubmit={onAddActivity}>
         <h1>Add new Activity:</h1>
         <div>
           <label>Name:</label>
-          <input name='nameInput' ></input>
+          <input name='name' ></input>
         </div>
         <div>
             <label>Good Weather Activity:</label>
-            <input type='checkbox'name='nameCheckbox'></input>
+            <input 
+            type='checkbox'
+            name='isForGoodWeather' 
+            checked={checked}
+            onChange={(e) => {setChecked(e.target.checked); console.log(e.target.checked)} }
+            >
+
+            </input>
         </div>
         <button type="submit">Submit</button>
       </form>
